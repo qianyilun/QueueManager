210439024ea24fa5fb4f85504dc54411dbfc5450
qianyilun <qianyilun@gmail.com>
2017-08-07 17:02:18 -0700

    build template

--- src/main/java/com/CaptureElement.java
+++ src/main/java/com/CaptureElement.java
@@ -2,6 +2,7 @@ package com;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -10,13 +11,24 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  */
 public class CaptureElement {
     public static void main(String[] args) {
+        // prevent java.lang.IllegalStateException
+        String key = "webdriver.chrome.driver";
+        String value = "/Users/Allen/Projects/QueueManager/src/chromedriver/chromedriver";
+        System.setProperty(key, value);
+
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("http://www.facebook.com");
-//        driver.findElement(By.xpath("/html/body/form/input[1]")).sendKeys("allen");
-//        driver.findElement(By.xpath("/html/body/form/input[2]")).sendKeys("allen");
-//        driver.findElement(By.xpath("/html/body/form/input[3]")).click();
 
+        String URL = "http://localhost:8080/myLogin/login";
+        driver.get(URL);
+        System.out.println(driver.getTitle());
 
+        // Enter into unassigned queue
+        WebElement element =  driver.findElement(By.xpath("/html/body/form/input[1]"));
+        element.sendKeys("allen");
+        driver.findElement(By.xpath("/html/body/form/input[2]")).sendKeys("allen");
+        driver.findElement(By.xpath("/html/body/form/input[3]")).click();
+
+        driver.quit();
     }
 }
--- src/main/java/com/allen.java
+++
@@ -1,7 +0,0 @@
-package com;
-
-/**
- * Created by yilunq on 07/08/17.
- */
-public class allen {
-}
